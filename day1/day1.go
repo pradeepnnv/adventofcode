7c3701163e8c81ba2eb0597773f64c92507bfcbf
pradeepnnv <pradeepnnv@gmail.com>
2024-12-03 13:54:53 -0600

    2024 day 1 part 2

--- day1/day1.go
+++ day1/day1.go
@@ -10,9 +10,52 @@ import (
 )
 
 func SolvePart1() int {
-	s := readInput("inputs/day1.txt")
-	list1 := make([]int, 0)
-	list2 := make([]int, 0)
+	list1, list2 := readInput("inputs/day1.txt")
+
+	var diff int
+	for i := 0; i < len(list1); i++ {
+		var d int
+		if list1[i] > list2[i] {
+			d = list1[i] - list2[i]
+		} else {
+			d = list2[i] - list1[i]
+		}
+		//log.Printf("difference between %d and %d is %d\n", list1[i], list2[i], d)
+		diff += d
+	}
+	return diff
+}
+
+func SolvePart2() (score int) {
+
+	list1, list2 := readInput("inputs/day1.txt")
+	//log.Println(list1, list2)
+	sm := make(map[int]int)
+	//prepare a score map of list2 items
+	for _, v := range list2 {
+		sm[v]++
+	}
+	//log.Println(sm)
+	for i := 0; i < len(list1); i++ {
+		s := sm[list1[i]]
+		//log.Printf("similarity score for %d  is %d", list1[i], s*list1[i])
+		score += s * list1[i]
+	}
+	return
+}
+
+func readInput(fn string) (list1, list2 []int) {
+	f, err := os.Open(fn)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer func(f *os.File) {
+		err := f.Close()
+		if err != nil {
+
+		}
+	}(f)
+	s := bufio.NewScanner(f)
 	for s.Scan() {
 		ln := strings.TrimSpace(s.Text())
 		d := strings.Split(ln, "   ")
@@ -39,26 +82,6 @@ func SolvePart1() int {
 	//log.Println(list1, list2)
 	sort.Ints(list1)
 	sort.Ints(list2)
-	//log.Println("Sorted lists", list1, list2)
-	var diff int
-	for i := 0; i < len(list1); i++ {
-		var d int
-		if list1[i] > list2[i] {
-			d = list1[i] - list2[i]
-		} else {
-			d = list2[i] - list1[i]
-		}
-		//log.Printf("difference between %d and %d is %d\n", list1[i], list2[i], d)
-		diff += d
-	}
-	return diff
-}
-
-func readInput(fn string) *bufio.Scanner {
-	f, err := os.Open(fn)
-	if err != nil {
-		log.Fatal(err)
-	}
-	scanner := bufio.NewScanner(f)
-	return scanner
+	//return scanner
+	return list1, list2
 }
--- main.go
+++ main.go
@@ -7,4 +7,5 @@ import (
 
 func main() {
 	log.Printf("Total difference is %d", day1.SolvePart1())
+	log.Printf("Similarity score is %d", day1.SolvePart2())
 }
